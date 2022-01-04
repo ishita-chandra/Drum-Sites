b8263abea543d5aefea832bcece431d30a024253
ishita-chandra <69865254+ishita-chandra@users.noreply.github.com>
2022-01-04 21:02:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,31 +29,31 @@ setTimeout(function()
 function makeSound(key){
 switch(key){
   case "w":
-    var crash=new Audio('sounds/crash.mp3');
+    var crash=new Audio('https://ishita-chandra.github.io/Drum-Sites/sounds/crash.mp3');
     crash.play();
    break;
   case "a":
-    var kick=new Audio('sounds/kick.mp3');
+    var kick=new Audio('https://ishita-chandra.github.io/Drum-Sites/sounds/kick.mp3');
     kick.play();
    break;
   case "s":
-    var snare=new Audio('sounds/snare.mp3');
+    var snare=new Audio('https://ishita-chandra.github.io/Drum-Sites/sounds/snare.mp3');
     snare.play();
    break;
   case "d":
-    var tom1=new Audio('sounds/tom-1.mp3');
+    var tom1=new Audio('https://ishita-chandra.github.io/Drum-Sites/sounds/tom-1.mp3');
     tom1.play();
    break;
   case "j":
-    var tom2=new Audio('sounds/tom-2.mp3');
+    var tom2=new Audio('https://ishita-chandra.github.io/Drum-Sites/sounds/tom-2.mp3');
     tom2.play();
    break;
   case "k":
-    var tom3=new Audio('sounds/tom-3.mp3');
+    var tom3=new Audio('https://ishita-chandra.github.io/Drum-Sites/sounds/tom-3.mp3');
     tom3.play();
    break;
   case "l":
-    var tom4=new Audio('sounds/tom-4.mp3');
+    var tom4=new Audio('https://ishita-chandra.github.io/Drum-Sites/sounds/tom-4.mp3');
     tom4.play();
    break;
   default:
